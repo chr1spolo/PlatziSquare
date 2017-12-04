b0f2610557676244ff6907f0bb5c75677c8a49f2
chr1spolo <polidavis28@gmail.com>
2017-12-03 20:41:22 -0500

    Adding routerLink and linkActive

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,6 +5,7 @@ import { AppPlacesComponent } from './places/places.component';
 
 const APP_ROUTES: Routes = [
   { path: '', component: AppPlacesComponent, pathMatch: 'full' },
+  { path: 'index', component: AppPlacesComponent },
   { path: 'details', component: AppDetailsComponent }
 ];
 
