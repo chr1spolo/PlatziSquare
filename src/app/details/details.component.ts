ffd3d4b3abd5f26a6168754200b0eb694473f0c9
chr1spolo <polidavis28@gmail.com>
2017-12-03 20:52:11 -0500

    Routes params

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
-export class AppDetailsComponent {}
+export class AppDetailsComponent {
+
+  constructor(private router: ActivatedRoute) {
+    console.log(this.router.snapshot.params['id']);
+  }
+}
--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -7,10 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppPlacesComponent {
   places: any = [
-    {plan: 'paid', nearBy: 1, distance: 1, active: true, nombre: 'Floristeria la gacela'},
-    {plan: 'free', nearBy: 1, distance: 3, active: true, nombre: 'Veterinaria huellitas felices'},
-    {plan: 'free', nearBy: 2, distance: 7, active: false, nombre: 'Drogas la rebaja'},
-    {plan: 'paid', nearBy: 6, distance: 30, active: true, nombre: 'Tienda el rebajón'}
+    {id: 1, plan: 'paid', nearBy: 1, distance: 1, active: true, nombre: 'Floristeria la gacela'},
+    {id: 2, plan: 'free', nearBy: 1, distance: 3, active: true, nombre: 'Veterinaria huellitas felices'},
+    {id: 3, plan: 'free', nearBy: 2, distance: 7, active: false, nombre: 'Drogas la rebaja'},
+    {id: 4, plan: 'paid', nearBy: 6, distance: 30, active: true, nombre: 'Tienda el rebajón'}
   ];
 
   constructor () {
